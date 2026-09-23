16b21aaf1d11a82a113088af5a5aabb946f03721
Santiago Ortega <tomasortega@unicauca,edu.co>
2026-09-22 23:29:11 -0500

    correcion de los pines activos en bajo

--- ejercicio1_FINAL/pkg_utilidades.vhd
+++ ejercicio1_FINAL/pkg_utilidades.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 package pkg_utilidades is
-    -- Función para convertir un dígito (0 a 9) a 7 segmentos (Cátodo Común)
+    -- Función para convertir un dígito (0 a 9) a 7 segmentos (Lógica Invertida / Ánodo Común)
     function bin_a_7seg(digito : integer range 0 to 9) return std_logic_vector;
 end package pkg_utilidades;
 
@@ -12,18 +12,19 @@ package body pkg_utilidades is
         variable seg : std_logic_vector(6 downto 0);
     begin
         -- Orden de segmentos: "gfedcba"
+        -- '0' = Segmento ENCENDIDO | '1' = Segmento APAGADO
         case digito is
-            when 0 => seg := "0111111";
-            when 1 => seg := "0000110";
-            when 2 => seg := "1011011";
-            when 3 => seg := "1001111";
-            when 4 => seg := "1100110";
-            when 5 => seg := "1101101";
-            when 6 => seg := "1111101";
-            when 7 => seg := "0000111";
-            when 8 => seg := "1111111";
-            when 9 => seg := "1101111";
-            when others => seg := "0000000";
+            when 0 => seg := "1000000";
+            when 1 => seg := "1111001";
+            when 2 => seg := "0100100";
+            when 3 => seg := "0110000";
+            when 4 => seg := "0011001";
+            when 5 => seg := "0100010";
+            when 6 => seg := "0000010";
+            when 7 => seg := "1111000";
+            when 8 => seg := "0000000";
+            when 9 => seg := "0010000";
+            when others => seg := "1111111"; -- Todos los segmentos apagados
         end case;
         return seg;
     end function bin_a_7seg;
